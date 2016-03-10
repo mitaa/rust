ac49c56af3722e9b66eca66adcced0abcd7ca1a1
mitaa <mitaa.ceb@gmail.com>
2016-03-10 13:56:00 +0100

    Update str.rs

--- src/libcollections/str.rs
+++ src/libcollections/str.rs
@@ -197,7 +197,7 @@ impl str {
         core_str::StrExt::len(self)
     }
 
-    /// Returns true if this slice has a length of zero bytes.
+    /// Returns `true` if this slice has a length of zero bytes.
     ///
     /// # Examples
     ///
